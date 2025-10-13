c6e8082ff3d6db911a7ae3f5f60b052517f506e6
sunmok <rlfhr9191@gmail.com>
2025-10-13 16:46:27 +0900

    feat:removeUntil

--- Stack_and_Queue/Q6_C_SQ.c
+++ Stack_and_Queue/Q6_C_SQ.c
@@ -111,7 +111,11 @@ int main()
 
 void removeUntil(Stack *s, int value)
 {
-/* add your code here */
+	while (!isEmptyStack(s)) {
+		if (value == peek(s))
+			return;
+		pop(s);
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////
